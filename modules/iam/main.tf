e2219682d9bd7917d028ec0a6721c48f752f9170
AnkushPatil45 <aankushpatil1989@gmail.com>
2025-11-22 12:59:10 -0500

    Used IAM Role and removed the previous role

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,37 +1,5 @@
-resource "aws_iam_role" "ec2_role" {
-  name = "${var.env_name}-EC2-Role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_policy" "s3_read" {
-  name = "${var.env_name}-S3ReadPolicy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = ["s3:GetObject"]
-      Resource = ["${var.bucket_arn}/*"]
-    }]
-  })
-}
-
-# Good code only attach the policy after creating both role and policy
-resource "aws_iam_role_policy_attachment" "attach" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = aws_iam_policy.s3_read.arn
-}
-
-
 resource "aws_iam_instance_profile" "profile" {
-  name = "${var.env_name}-InstanceProfile"
-  role = aws_iam_role.ec2_role.name
+  name = "LabRole"
+  role = var.existing_role_name
 }
+

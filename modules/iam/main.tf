33aa289227b66f30c88fa058b18995ac236d8a03
AnkushPatil45 <aankushpatil1989@gmail.com>
2025-11-22 12:34:55 -0500

    feat(iam) created a new policy and profile on iam that can access S3 and also use all ec2 features

---
+++ modules/iam/main.tf
@@ -0,0 +1,37 @@
+resource "aws_iam_role" "ec2_role" {
+  name = "${var.env_name}-EC2-Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "s3_read" {
+  name = "${var.env_name}-S3ReadPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = ["s3:GetObject"]
+      Resource = ["${var.bucket_arn}/*"]
+    }]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.s3_read.arn
+}
+
+
+resource "aws_iam_instance_profile" "profile" {
+  name = "${var.env_name}-InstanceProfile"
+  role = aws_iam_role.ec2_role.name
+}

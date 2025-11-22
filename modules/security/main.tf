b69444101453f795dbdaa19741a7018a493f8958
Generalfocus <generalfocus1@gmail.com>
2025-11-22 09:45:37 -0500

    feat(security): limit ec2 security group to only allow alb

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -17,3 +17,21 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
+resource "aws_security_group" "web_sg" {
+  name   = "${var.env_name}-WEB-SG"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}

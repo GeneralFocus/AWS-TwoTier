5f98d595c1d1e6f04e7d8fb4c9a76d21087dd4ed
Generalfocus <generalfocus1@gmail.com>
2025-11-24 16:54:49 -0500

    Update modules/launch-template/main.tf

---
+++ modules/launch-template/main.tf
@@ -0,0 +1,24 @@
+resource "aws_launch_template" "lt" {
+  name_prefix   = "${var.env_name}-LT"
+  image_id      = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [var.web_sg]
+  }
+
+  user_data = base64encode(templatefile("${path.module}/user_data.sh", {
+    bucket = var.bucket
+  }))
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.env_name}-instance"
+      Env  = var.env_name
+    }
+  }
+
+}

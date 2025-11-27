3110bf35a4c7323d56c2968521472b84713d64d0
AnkushPatil45 <aankushpatil1989@gmail.com>
2025-11-27 18:56:45 -0500

    Fix: Added type constraints (string/number/list) to all variables in modules/asg/variables.tf

--- modules/asg/variables.tf
+++ modules/asg/variables.tf
@@ -1,7 +1,21 @@
-variable "env_name" {}
-variable "min_size" {}
-variable "max_size" {}
-variable "desired_capacity" {}
-variable "lt_id" {}
-variable "tg_arn" {}
-variable "private_subnets" {}
+variable "env_name" {
+  type = string # Corrected
+}
+variable "min_size" {
+  type = number # Corrected
+}
+variable "max_size" {
+  type = number # Corrected
+}
+variable "desired_capacity" {
+  type = number # Corrected
+}
+variable "lt_id" {
+  type = string # Corrected
+}
+variable "tg_arn" {
+  type = string # Corrected
+}
+variable "private_subnets" {
+  type = list(string) # Corrected
+}

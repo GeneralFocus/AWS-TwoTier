c1e5c6a9e3f06fa3f0e408ea551125a3d4835209
AnkushPatil45 <aankushpatil1989@gmail.com>
2025-11-27 18:56:46 -0500

    Fix: Added 'type = string' to 'env_name' and 'vpc_cidr' in modules/networking/variables.tf

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,5 +1,9 @@
-variable "env_name" {}
-variable "vpc_cidr" {}
+variable "env_name" {
+  type = string # Added 'type' attribute
+}
+variable "vpc_cidr" {
+  type = string # Added 'type' attribute
+}
 
 variable "public_subnets" {
   type = map(string)

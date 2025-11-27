dae5cf02b4df34428a7e88bfe472fcd81605c384
AnkushPatil45 <aankushpatil1989@gmail.com>
2025-11-27 18:56:45 -0500

    Fix: Added required Terraform and AWS provider versions to modules/iam/main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,3 +1,15 @@
+# Added a 'terraform' configuration block to define version requirements
+terraform {
+  required_version = ">= 1.0" # Specify the required Terraform version
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Specify a required version for the AWS provider (e.g., v5.x)
+    }
+  }
+}
+
 resource "aws_iam_instance_profile" "profile" {
   name = "LabRole"
   role = var.existing_role_name
